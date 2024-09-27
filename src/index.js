c9723db74fa987d86d3d6880242ac701bdc8ae92
Kate465 <kategokeborate@gmail.com>
2024-09-27 22:46:25 +0100

    injected HTML from javascript

--- src/index.js
+++ src/index.js
@@ -57,5 +57,32 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement = addEventListener("submit", handleSearchSubmit);
+
+function displayForecast() {
+  let days = ["thur", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `  <div class="weather-forecast">
+      <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌧️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15°</strong></div>
+               <div class="weather-forecast-temperature">9°</div>
+              </div>
+              </div>
+            </div>
+            `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+displayForecast();
